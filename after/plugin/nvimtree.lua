b10e24f1ea27edcbf7f6a2ec25ee23302d0f6c6e
QDoa <damilareadeniyi@gmail.com>
2024-08-31 05:41:47 -0600

    Change package manager to lazy

--- after/plugin/nvimtree.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set('n', '<leader>n', '<cmd>NvimTreeFocus<cr>')
-vim.keymap.set('n', '<C-n>', '<cmd>NvimTreeOpen<cr>')
-vim.keymap.set('n', '<C-t>', '<cmd>NvimTreeToggle<cr>')
-vim.keymap.set('n', '<C-f>', '<cmd>NvimTreeFind<cr>')
--- lua/doa/init.lua
+++
@@ -1,2 +0,0 @@
-require("doa.set")
-require("doa.remap")
