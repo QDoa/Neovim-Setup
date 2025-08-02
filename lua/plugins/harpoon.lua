9c2cb54cb3fe168c07291a81075399f7416f1e25
Damilare Adeniyi <Damilare-Oluwa_Adeniyi@mentor.com>
2025-08-01 21:47:30 -0700

    Added new plugins

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,11 +1,20 @@
 return {
-    'theprimeagen/harpoon',
-    branch = "harpoon2",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    keys = {
-        {'<leader>a', ':lua require("harpoon.mark").add_file()<cr>', mode = 'n'},
-        {'<C-e>', ':lua require("harpoon.ui").toggle_quick_menu()<cr>', mode = 'n'},
-    },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        opts = {},
+        config = function ()
+            local harpoon = require("harpoon")
+
+            -- REQUIRED
+            harpoon:setup()
+            -- REQUIRED
+
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        end
+    }
 }
